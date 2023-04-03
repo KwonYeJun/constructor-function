574d56f17bd4d5d77db1650b24c9305a37708d54
gwonyejun <kyjuni1059@gmail.com>
2023-04-03 18:06:46 +0900

    마무리 소켓

--- server/server-main.js
+++ server/server-main.js
@@ -1,5 +1,10 @@
 import http from 'http';
 import db from './db-sever.js';
+import fs from 'fs';
+import io from 'socket.io-client';
+const socket = io('http://localhost');
+
+
 
 db.connect((err) => {
   if (err) { console.log('실패') }
@@ -9,36 +14,49 @@ db.connect((err) => {
 }); //! db서버 열기
 
 
-db.query('show tables' , (err,re,q) =>
-{
-  if(err){console.log(err)}
+db.query('show tables', (err, re, q) => {
+  if (err) { console.log(err) }
   console.log(re);
 })
-// const server = http.createServer((request, response) => {
+const server = http.createServer((request, response) => {
 
-//   let a = true;
-//   switch (true) {
-//     case (a = true):
-//       console.log( '비교하려는 값보다 작습니다.' );
-//       break;
-//     case 4:
-//       console.log( '비교하려는 값과 일치합니다.' );
-//       break;
-//     case 5:
-//       console.log( '비교하려는 값보다 큽니다.' );
-//       break;
-//     default:
-//       console.log( "어떤 값인지 파악이 되지 않습니다." );
-//   }
-
-// })
+  const testfile = fs.readFileSync('./test.html', { encoding: 'utf-8' });
+  if (request.method === "GET") {
+    response.statusCode = 200;
+    response.setHeader('Content-Type', 'text/html');
+    response.end(testfile);
+  }
 
 
-// server.listen(1234, function (err) {
-//   if (err) {
-//     console.log('서버 연결 실패');
-//   }
-//   else {
-//     console.log('서버 연결 성공');
-//   }
-// });
+})
+io.on('connect', (socket) => {
+  const ip = socket.request.headers['x-forwarded-for'] || socket.request.connection.remoteAddress;
+  console.log(`클라이언트 연결 성공 - 클라이언트IP: ${ip}, 소켓ID: ${socket.id}`);
+
+  // 2) 연결 종료 이벤트: "매개변수로 들어온 socket"으로 처리해야 함 주의!
+  socket.on('disconnect', (reason) => {
+    console.log(reason);
+    console.log(`연결 종료 - 클라이언트IP: ${ip}, 소켓ID: ${socket.id}`)
+  });
+
+  // 3) 에러 발생 이벤트: "매개변수로 들어온 socket"으로 처리해야 함 주의!
+  socket.on('error', (error) => {
+    console.log(`에러 발생: ${error}`);
+  })
+
+  // 4) 클라이언트에서 보낸 이벤트 처리. 클라이언트에서 "client_msg" 이름으로 보낸 데이터 수신
+  socket.on('client_msg', (data) => {
+    console.log(`클라이언트에서 보낸 메시지 수신: ${data}`);
+
+    //클라이언트에게 "server_msg" 이름으로 데이터 전송
+    socket.emit('server_msg', `[${socket.id}]소켓 서버에서 보낸 메시지입니다.`);
+  })
+});
+server.listen(1234, function (err) {
+  if (err) {
+    console.log('서버 연결 실패');
+  }
+  else {
+    console.log('서버 연결 성공');
+  }
+});
